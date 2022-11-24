1536ec12a635b68c6d3f63632dd687de89d4add8
chargerneeded <umi0236@kakao.com>
2022-11-24 20:43:24 +0900

    Initial Commit

---
+++ SlackLogin/SlackLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlackLoginApp.swift
+//  SlackLogin
+//
+//  Created by 강경민 on 2022/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SlackLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
